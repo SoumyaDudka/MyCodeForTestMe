7de937f6406361fec6d809ecc92f1a455bd18b63
SoumyaDudka <54269596+SoumyaDudka@users.noreply.github.com>
2019-08-21 10:30:05 +0530

    Update MyRunner.java

--- src/test/java/myStepDefinitions/MyRunner.java
+++ src/test/java/myStepDefinitions/MyRunner.java
@@ -6,7 +6,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium_WS\\Cucumber_demo1\\My_features\\demotours_login.feature",glue = "myStepDefinitions")
+@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium_WS\\Cucumber_demo1\\My_features\\demotours_login.feature",
+                 glue = "myStepDefinitions",tags = {"@Regreesion,@Smoke"},
+                plugin = {"json:target/cucumber.json"})
 
 
 public class MyRunner {
